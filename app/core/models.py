47e8f0bfe32ff83ab974df18b4a697a07f499a26
António Gomes <antonio.gomes@nrthree.com>
2024-11-27 07:43:33 +0000

    API Documentation

--- app/core/models.py
+++ app/core/models.py
@@ -1,8 +1,9 @@
 """
 Database models.
 """
+
 from django.db import models
-from django.contrib.auth.models import(
+from django.contrib.auth.models import (
     AbstractBaseUser,
     BaseUserManager,
     PermissionsMixin,
@@ -13,7 +14,7 @@ class UserManager(BaseUserManager):
     """Manager for users."""
 
     def create_user(self, email, password=None, **extra_fields):
-        """Create, save and return a new user"""
+        """Create, save and return a new user."""
         if not email:
             raise ValueError('User must have an email address.')
         user = self.model(email=self.normalize_email(email), **extra_fields)
@@ -23,7 +24,7 @@ class UserManager(BaseUserManager):
         return user
 
     def create_superuser(self, email, password):
-        """Create and return a new superuser"""
+        """Create and return a new superuser."""
         user = self.create_user(email, password)
         user.is_staff = True
         user.is_superuser = True
